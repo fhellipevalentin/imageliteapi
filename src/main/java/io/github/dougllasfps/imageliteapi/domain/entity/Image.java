ab4d7ac89979e46de6c1f26f38a27e6a1b6aa709
fhellipevalentin <fhellipereis@outlook.com>
2025-09-15 17:35:53 -0300

    int. bco de dados e criacao de tabela - repo de imagens

---
+++ src/main/java/io/github/dougllasfps/imageliteapi/domain/entity/Image.java
@@ -0,0 +1,36 @@
+package io.github.dougllasfps.imageliteapi.domain.entity;
+
+import io.github.dougllasfps.imageliteapi.domain.enums.ImageExtension;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_image")
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+    private Long size;
+    @Enumerated(EnumType.STRING)
+    private ImageExtension extension;
+    @CreatedDate
+    private LocalDateTime uploadDate;
+    private String tags;
+    @Lob
+    private byte[] file;
+}
