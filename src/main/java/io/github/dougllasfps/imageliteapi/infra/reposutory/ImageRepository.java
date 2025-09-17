28616454517cb0fe8bf9037d1ed15424c4e103db
fhellipevalentin <fhellipereis@outlook.com>
2025-09-17 10:32:45 -0300

    Adicionando capacidade de executar Specifications no repositorio de imagens

--- src/main/java/io/github/dougllasfps/imageliteapi/infra/reposutory/ImageRepository.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/infra/reposutory/ImageRepository.java
@@ -1,9 +1,18 @@
 package io.github.dougllasfps.imageliteapi.infra.reposutory;
 
 import io.github.dougllasfps.imageliteapi.domain.entity.Image;
+import io.github.dougllasfps.imageliteapi.domain.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ImageRepository extends JpaRepository<Image, String> {
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query) {
+        return findAll();
+    }
+
 }
